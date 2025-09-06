8929dc88a0535a9ebff7777073dae0681a8a3f99
Ítalo Maciel <italo.souza@cobasi.com.br>
2025-09-06 17:46:24 -0400

    implementa nivel iniciante

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,7 +4,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+int main()
+{
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
@@ -25,7 +26,7 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
-    
+
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
     // 0 1 1 1 0
@@ -35,6 +36,71 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+
+    int tabulereiro[10][10];
+    int coordNavioHorizontal[2] = {5, 3};
+    int coordNavioVertical[2] = {2, 3};
+    char colunas[10] = "ABCDEFGHIJ";
+
+    // valida posição do navio
+    if (coordNavioHorizontal[1] + 2 > 10 || coordNavioHorizontal[1] - 2 < 0)
+    {
+        printf("\nPosição do navio ultrapssou limites horizontais\n");
+        return 0;
+    }
+
+    // verifica colisão
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (coordNavioHorizontal[1] + i == coordNavioVertical[1] && coordNavioHorizontal[0] == coordNavioVertical[0] + j)
+            {
+                printf("\nOs navios colidiram\n");
+                return 0;
+            }
+        }
+    }
+
+    if (coordNavioVertical[0] + 2 > 10 || coordNavioVertical[0] - 2 < 0)
+    {
+        printf("\nPosição do navio ultrapssou limites verticais\n");
+        return 0;
+    }
+
+    // popular tabulereiro
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            tabulereiro[i][j] = 0;
+        }
+    }
+
+    // posiciona navios no tabulereiro
+    for (int i = 0; i < 3; i++)
+    {
+        tabulereiro[coordNavioHorizontal[0] - 1][(coordNavioHorizontal[1] - 1) + i] = 1;
+        tabulereiro[(coordNavioVertical[0] - 1) + i][coordNavioVertical[1] - 1] = 1;
+    }
+
+    // monta colunas
+    printf("   ");
+    for (int i = 0; i < 10; i++)
+    {
+        printf(" %c ", colunas[i]);
+    }
+
+    printf("\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%2d ", i + 1); // alinhamento
+        for (int j = 0; j < 10; j++)
+        {
+            printf(" %d ", tabulereiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
